549042c698178fcbdc0b553e669e35907c751273
Matej Straka <strakammm@gmail.com>
2022-08-21 14:51:28 +0200

    first commit nvim awesomeWM sxhkd alacritty

---
+++ awesome/binding/clientkeys.lua
@@ -0,0 +1,35 @@
+-- Standard Awesome library
+local gears = require("gears")
+local awful = require("awful")
+-- Custom Local Library
+-- local titlebar = require("anybox.titlebar")
+
+local _M = {}
+local modkey = RC.vars.modkey
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+function _M.get()
+  local clientkeys = gears.table.join(
+    awful.key({ modkey,           }, "f",
+      function (c)
+        c.fullscreen = not c.fullscreen
+        c:raise()
+      end,
+      {description = "toggle fullscreen", group = "client"}),
+    awful.key({ modkey,           }, "q",      function (c) c:kill()                         end,
+              {description = "close", group = "client"}),
+    awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end,
+              {description = "move to master", group = "client"}),
+    awful.key({ modkey,           }, "o",      function (c) c:move_to_screen()               end,
+              {description = "move to screen", group = "client"}),
+    awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end,
+              {description = "toggle keep on top", group = "client"})
+  )
+
+  return clientkeys
+end
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+return setmetatable({}, { __call = function(_, ...) return _M.get(...) end })
---
+++ awesome/deco/statusbar.lua
@@ -0,0 +1,94 @@
+-- Standard awesome library
+local gears = require("gears")
+local awful = require("awful")
+
+-- Wibox handling library
+local wibox = require("wibox")
+
+-- Custom Local Library: Common Functional Decoration
+local deco = {
+    wallpaper = require("deco.wallpaper"),
+    taglist   = require("deco.taglist"),
+    tasklist  = require("deco.tasklist")
+}
+
+-- awesome-wm-widgets
+local batteryarc_widget = require("awesome-wm-widgets.batteryarc-widget.batteryarc")
+local brightness_widget = require("awesome-wm-widgets.brightness-widget.brightness")
+local logout_menu_widget = require("awesome-wm-widgets.logout-menu-widget.logout-menu")
+local spotify_widget = require("awesome-wm-widgets.spotify-widget.spotify")
+local volume_widget = require('awesome-wm-widgets.volume-widget.volume')
+
+
+
+
+local taglist_buttons  = deco.taglist()
+local tasklist_buttons = deco.tasklist()
+
+local _M = {}
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+-- {{{ Wibar
+-- Create a textclock widget
+mytextclock = wibox.widget.textclock()
+
+awful.screen.connect_for_each_screen(function(s)
+    -- Wallpaper
+    set_wallpaper(s)
+
+    -- Create a promptbox for each screen
+    s.mypromptbox = awful.widget.prompt()
+
+    -- Create an imagebox widget which will contain an icon indicating which layout we're using.
+    -- We need one layoutbox per screen.
+    s.mylayoutbox = awful.widget.layoutbox(s)
+    s.mylayoutbox:buttons(gears.table.join(
+        awful.button({ }, 1, function () awful.layout.inc( 1) end),
+        awful.button({ }, 3, function () awful.layout.inc(-1) end),
+        awful.button({ }, 4, function () awful.layout.inc( 1) end),
+        awful.button({ }, 5, function () awful.layout.inc(-1) end)
+    ))
+
+    -- Create a taglist widget
+    s.mytaglist = awful.widget.taglist {
+        screen  = s,
+        filter  = awful.widget.taglist.filter.all,
+        buttons = taglist_buttons
+    }
+
+    -- Create a tasklist widget
+    s.mytasklist = awful.widget.tasklist {
+        screen  = s,
+        filter  = awful.widget.tasklist.filter.currenttags,
+        buttons = tasklist_buttons
+    }
+
+    -- Create the wibox
+    s.mywibox = awful.wibar({ position = "top", screen = s })
+
+    -- Add widgets to the wibox
+    s.mywibox:setup {
+        layout = wibox.layout.align.horizontal,
+        { -- Left widgets
+            layout = wibox.layout.fixed.horizontal,
+            s.mytaglist,
+            s.mypromptbox,
+        },
+        s.mytasklist, -- Middle widget
+        { -- Right widgets
+            layout = wibox.layout.fixed.horizontal,
+            spotify_widget(),
+            volume_widget({
+                device = 'default',
+            }),
+            mykeyboardlayout,
+            batteryarc_widget(),
+            brightness_widget(),
+            wibox.widget.systray(),
+            mytextclock,
+            logout_menu_widget(),
+        },
+    }
+end)
+-- }}}
